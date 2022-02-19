e7ba63b7920e5cf2f8f5af2c4a293e7c4c4461c8
ChSeven <32443773+ChSeven@users.noreply.github.com>
2022-02-19 19:06:28 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,3 +42,60 @@ var swiper = new Swiper(".food-slider", {
       },
    },
 });
+let previewContainer = document.querySelector('.food-preview-container');
+let previewBox = previewContainer.querySelectorAll('.food-preview');
+
+document.querySelectorAll('.food .slide').forEach(food =>{
+   food.onclick = () =>{
+      previewContainer.style.display = 'flex';
+      let name = food.getAttribute('data-name');
+      previewBox.forEach(preveiw =>{
+         let target = preveiw.getAttribute('data-target');
+         if(name == target){
+            preveiw.classList.add('active');
+         }
+      });
+   };
+});
+
+previewContainer.querySelector('#close-preview').onclick = () =>{
+   previewContainer.style.display = 'none';
+   previewBox.forEach(close =>{
+      close.classList.remove('active');
+   });
+};
+
+var swiper = new Swiper(".menu-slider", {
+   grabCursor:true,
+   loop:true,
+   autoHeight:true,
+   centeredSlides:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+   grabCursor:true,
+   loop:true,
+   centeredSlides:true,
+   autoHeight:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      700: {
+        slidesPerView: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+      },
+   },
+});
